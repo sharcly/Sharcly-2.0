08432089100232d9f66e1a0dd247d3c712e390f8
osama <osama@jetquin.me>
2026-05-01 12:27:26 -0700

    feat: implement product management dashboard and update database connection string environment variables

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "@prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL,
+    url: process.env.POSTGRES_URL_NON_POOLING || process.env.DIRECT_URL || process.env.POSTGRES_URL || process.env.DATABASE_URL,
   },
 });
 
